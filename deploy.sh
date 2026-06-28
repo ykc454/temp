aff5da8c4834e25bdc0c7ff385b828fd578f4a14
ykc454 <yashchaudhari1605@gmail.com>
2026-06-28 17:38:14 +0530

    rollback

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,64 @@
 #!/bin/bash
 
-echo "Building Docker image..."
+ENVIRONMENT=$1
+VERSION=$2
+REGISTRY=$3
 
-docker build -t flask-api:v0.0.0 .
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./deploy.sh <environment> <version> <image_registry>"
+    exit 1
+fi
 
-echo "Running Container..."
+case "$ENVIRONMENT" in
+    dev|staging|prod)
+        ;;
+    *)
+        echo "Invalid environment."
+        exit 1
+        ;;
+esac
 
-docker run -p 5000:5000 flask-api:v0.0.0
+LOG_FILE="deployment.log"
+
+echo "Deployment Started: $(date)" >> "$LOG_FILE"
+
+echo "Deploying version $VERSION" >> "$LOG_FILE"
+
+
+command -v kubectl >/dev/null 2>&1 || {
+    echo "kubectl not found."
+    exit 1
+}
+
+command -v helm >/dev/null 2>&1 || {
+    echo "helm not found."
+    exit 1
+}
+
+
+helm upgrade --install flask-api ./helm/flask-api \
+    --set image.repository=$REGISTRY/flask-api \
+    --set image.tag=$VERSION \
+    --wait
+
+
+helm upgrade --install flask-api ./helm/flask-api \
+    --set image.repository=docker.io/yash/flask-api \
+    --set image.tag=v1.0.5 \
+    --wait
+
+kubectl port-forward service/flask-api-service 5000:5000 &
+sleep 5
+
+curl --fail http://localhost:5000/health
+
+if curl --fail http://localhost:5000/health
+then
+    echo "Smoke Test Passed"
+    echo "Deployment Successful" >> deployment.log
+else
+    echo "Smoke Test Failed"
+    helm rollback flask-api
+    echo "Rollback Executed" >> deployment.log
+    exit 1
+fi
