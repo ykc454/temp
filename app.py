2edb6b006f8b0b8245c7e7d18790fc28d40cf17f
ykc454 <yashchaudhari1605@gmail.com>
2026-06-28 15:54:58 +0530

    version added

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
 @app.route("/health")
 def health():
     return jsonify({
-        "status": "soham"
+        "status": "healthy"
     })
 
 @app.route("/api/users")
--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 echo "Building Docker image..."
 
-docker build -t flask-api:latest .
+docker build -t flask-api:v0.0.0 .
 
 echo "Running Container..."
 
-docker run -p 5000:5000 flask-api:latest
+docker run -p 5000:5000 flask-api:v0.0.0
